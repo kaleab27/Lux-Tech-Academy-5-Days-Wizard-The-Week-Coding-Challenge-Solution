3393dc36e14611c206a7e83737236b5988f8564b
kaleab <60797196+kaleab27@users.noreply.github.com>
2021-05-07 19:42:04 +0300

    Update Day5.js

--- Day5.js
+++ Day5.js
@@ -1,3 +1,4 @@
 // Detect if two integers have opposite signs
 
 let compareIntegerSign = (num1, num2) => num1 > 0 && num2 > 0 || num1 < 0 && num2 < 0 ? 'SAME SIGN' : 'OPPOSITE SIGN'
+let _compareIntegerSign = (num1, num2) => num1 === num2 ? 'SAME SIGN' : 'OPPOSITE SIGN'
